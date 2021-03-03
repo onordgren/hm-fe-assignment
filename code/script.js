47a5626b7c438ca68021cf6056dd57cbb085d211
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2021-03-04 00:26:08 +0100

    added search for google books api

--- code/script.js
+++ code/script.js
@@ -1,6 +1,29 @@
-//import "./styles.css";
 
-// document.getElementById("app").innerHTML = `
-// <h1>Hello Vanilla!</h1>
+const books = document.getElementsByClassName("book");
+const button = document.getElementById("click-button");
+const input = document.getElementById("input");
 
-// `;
+button.addEventListener("click", () => {
+  const inputValue = input.value;
+  const API_KEY = "AIzaSyBFBmCkfIDsxbWqPyfROe19X3Izi-x9LMk";
+  const API_URL = `https://www.googleapis.com/books/v1/volumes?q="${inputValue}"&langRestrict=us&key=${API_KEY}`;
+
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const newBooks = data.items.map((book) => {
+        const author = book.volumeInfo.authors;
+        const title = book.volumeInfo.title;
+        const description = book.volumeInfo.description;
+        //console.log(author, title, description);
+
+        return { author, title, description };
+      });
+      newBooks.forEach((item, index) => {
+        books[index].querySelector(".title").innerText = item.title;
+        books[index].querySelector(".author").innerText = item.author;
+        books[index].querySelector(".description").innerText = item.description;
+      });
+    });
+  input.value = "";
+});
