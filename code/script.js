dc14b66b480cbbde931412cebbb74d0e94ad9c05
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2021-03-05 12:17:25 +0100

    created the autocomplete logic

--- code/script.js
+++ code/script.js
@@ -1,30 +1,53 @@
+const search = document.getElementById("input");
+const matchList = document.getElementById("match-list");
+//const API_KEY = config.apiKey;
+const API_KEY = "AIzaSyBFBmCkfIDsxbWqPyfROe19X3Izi-x9LMk";
+const inputValue = search.value;
+//const inputValue = input.value;
+//const API_URL = `https://www.googleapis.com/books/v1/volumes?q="harry potter"key=${API_KEY}`;
+//const API_URL = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
+const API_URL =
+  "https://www.googleapis.com/books/v1/volumes?q=search+terms&maxResults=40";
+//const API_URL = `https://www.googleapis.com/books/v1/volumes?q="${inputValue}"&langRestrict=us&key=${API_KEY}`;
 
-const books = document.getElementsByClassName("book");
-const button = document.getElementById("click-button");
-const input = document.getElementById("input");
+//search books and filter
+const searchBooks = async (searchText) => {
+  const res = await fetch(API_URL);
+  const data = await res.json();
+  console.log(data);
+  const newBooks = data.items.map((book) => {
+    //const author = book.volumeInfo.authors;
+    const title = book.volumeInfo.title;
+    //console.log(title);
+    return { title };
+  });
 
-button.addEventListener("click", () => {
-  const inputValue = input.value;
-  const API_KEY = config.apiKey;
-  //const API_KEY = "AIzaSyBFBmCkfIDsxbWqPyfROe19X3Izi-x9LMk";
-  const API_URL = `https://www.googleapis.com/books/v1/volumes?q="${inputValue}"&langRestrict=us&key=${API_KEY}`;
+  let matches = newBooks.filter((book) => {
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return book.title.match(regex);
+  });
+  //console.log(matches);
 
-  fetch(API_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      const newBooks = data.items.map((book) => {
-        const author = book.volumeInfo.authors;
-        const title = book.volumeInfo.title;
-        const description = book.volumeInfo.description;
-        //console.log(author, title, description);
+  if (searchText.length === 0) {
+    matches = [];
+    matchList.innerHTML = "";
+  }
+  showResults(matches);
+};
 
-        return { author, title, description };
-      });
-      newBooks.forEach((item, index) => {
-        books[index].querySelector(".title").innerText = item.title;
-        books[index].querySelector(".author").innerText = item.author;
-        books[index].querySelector(".description").innerText = item.description;
-      });
-    });
-  input.value = "";
-});
+//Show results in HTML
+const showResults = (matches) => {
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        (match) => `
+    <div class="card">
+    <h4>${match.title}</h4>
+    <div>`
+      )
+      .join("");
+    matchList.innerHTML = html;
+  }
+};
+search.addEventListener("input", () => searchBooks(search.value));
+
