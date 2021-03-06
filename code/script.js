811c69e3505f3012c3c3804384b7bcff4ae4ec3e
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2021-03-06 15:17:10 +0100

    fixed delete item logic and clear all

--- code/script.js
+++ code/script.js
@@ -2,30 +2,23 @@ const search = document.getElementById("input");
 const matchList = document.getElementById("match-list");
 const historyList = document.getElementById("history-list");
 const searchButton = document.getElementById("search-button");
-const deleteButton = document.getElementById("btn-delete");
 const clearAllButton = document.getElementById("btn-clear");
-//const API_KEY = config.apiKey;
 const API_URL =
   "https://www.googleapis.com/books/v1/volumes?q=search+terms&maxResults=40";
-//const API_URL = `https://www.googleapis.com/books/v1/volumes?q="${inputValue}"&langRestrict=us&key=${API_KEY}`;
 
 //search books and filter
+//autocomplete logic
 const searchBooks = async (searchText) => {
   const res = await fetch(API_URL);
   const data = await res.json();
-  console.log(data);
   const newBooks = data.items.map((book) => {
     const title = book.volumeInfo.title;
-    //console.log(title);
     return { title };
   });
-
   let matches = newBooks.filter((book) => {
     const regex = new RegExp(`^${searchText}`, "gi");
     return book.title.match(regex);
   });
-  //console.log(matches);
-
   if (searchText.length === 0) {
     matches = [];
     matchList.innerHTML = "";
@@ -33,9 +26,23 @@ const searchBooks = async (searchText) => {
   showResults(matches);
 };
 
+//Show results in HTML for search items
+const showResults = (matches) => {
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        (match) => `
+    <h4>${match.title}</h4>`
+      )
+      .join("");
+    matchList.innerHTML = html;
+  }
+};
+search.addEventListener("input", () => searchBooks(search.value));
+
 //add search history item
 searchButton.addEventListener("click", () => {
-  //create a div with a paragraph, a delete button and a time stamp every time a user clicks the Search button
+  //create a div with a paragraph, a delete button and a time stamp every time a user clicks on the Search button
   let input = search.value;
   if (input === "") {
     alert("please add a title");
@@ -62,41 +69,21 @@ searchButton.addEventListener("click", () => {
     newDiv.appendChild(time);
     newDiv.appendChild(button);
   }
-
   search.value = "";
 });
-
+//function that deletes a history item
 const deleteItem = (e) => {
-  console.log(e.target);
-  if (e.target.className === "history-item") {
-    e.target.remove();
+  if (e.target.className === "btn-delete") {
+    e.target.parentNode.remove();
   }
 };
 historyList.addEventListener("click", deleteItem);
 
+//function that deletes the whole history
 const clearList = (e) => {
-  const items = document.querySelectorAll("history-item");
+  const items = document.querySelectorAll(".history-item");
   items.forEach((item) => {
     item.remove();
   });
-  console.log(items);
 };
-
-window.onload = function () {
-  clearAllButton.addEventListener("click", clearList);
-};
-
-//Show results in HTML for search items
-const showResults = (matches) => {
-  if (matches.length > 0) {
-    const html = matches
-      .map(
-        (match) => `
-    <h4>${match.title}</h4>`
-      )
-      .join("");
-    matchList.innerHTML = html;
-  }
-};
-
-search.addEventListener("input", () => searchBooks(search.value));
+clearAllButton.addEventListener("click", clearList);
